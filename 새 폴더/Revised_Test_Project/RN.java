8e7d79509c462c4a7d38f67509783eacf3cb721a
Nestserka <78704791+Nestserka@users.noreply.github.com>
2021-02-09 11:34:53 +0900

    Add files via upload

---
+++ 새 폴더/Revised_Test_Project/RN.java
@@ -0,0 +1,84 @@
+package Revised_Test_Project;
+
+import java.util.*;
+
+
+public class RN {
+
+    public static boolean isRoman(String s) {
+        return s.matches("[CLXVI]+");
+    }
+
+
+    public static int romanToArabic(String value) {
+        String number = value.toUpperCase();
+        List<RomanNumbers> numbersList = RomanNumbers.getReverseSortedValues();
+        int result = 0;
+
+        for (int i = 0; number.length() > 0; ) {
+            RomanNumbers sym = numbersList.get(i);
+            if (number.startsWith(sym.name())) {
+                result += sym.getValue();
+                number = number.substring(sym.name().length());
+            } else {
+                i++;
+            }
+        }
+
+        return result;
+    }
+
+
+    public static String arabicToRoman(int number) {
+        List<RomanNumbers> numbersList = RomanNumbers.getReverseSortedValues();
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; number > 0; ) {
+            RomanNumbers sym = numbersList.get(i);
+
+            if (sym.getValue() <= number) {
+                result.append(sym.name());
+                number -= sym.getValue();
+            } else {
+                i++;
+            }
+        }
+
+        return result.toString();
+    }
+
+    enum RomanNumbers {
+        I(1),
+        IV(4),
+        V(5),
+        IX(9),
+        X(10),
+        XL(40),
+        L(50),
+        XC(90),
+        C(100);
+
+        private final int value;
+
+        RomanNumbers(int value) {
+            this.value = value;
+        }
+
+
+        public int getValue() {
+            return value;
+        }
+
+
+        public static ArrayList<RomanNumbers> getReverseSortedValues() {
+            ArrayList<RomanNumbers> reversedValues = new ArrayList<>(9);
+            reversedValues.addAll(Arrays.asList(RomanNumbers.values()));
+            Collections.reverse(reversedValues);
+
+            return reversedValues;
+        }
+
+    }
+
+}
+
---
+++ 새 폴더/com/Habsida/ErrorEnters/OperationIsNotPossible.java
@@ -0,0 +1,19 @@
+package com.Habsida.ErrorEnters;
+
+import Revised_Test_Project.ErrorEnters.IkindlyAccept;
+
+public class OperationIsNotPossible extends IkindlyAccept {
+    public OperationIsNotPossible(String s) {
+
+    }
+
+    public OperationIsNotPossible() {
+
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+
+}
+}
